4764bc1aa2bb74713309733a123993011b2776cd
Iann29 <iansingshau@gmail.com>
2024-05-22 20:31:47 -0300

    Testando em outras telas

--- src/Register.js
+++ src/Register.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './Register.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon
-import { faUser } from '@fortawesome/free-solid-svg-icons'; // Import the faUser icon
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faUser } from '@fortawesome/free-solid-svg-icons'; 
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -15,7 +15,7 @@ const Register = () => {
   });
 
   const [message, setMessage] = useState('');
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate(); 
 
   const { nome, email, senha, cpf, telefone } = formData;
 
@@ -29,8 +29,8 @@ const Register = () => {
       setMessage('Registro concluído com sucesso!');
       
       setTimeout(() => {
-        navigate('/login'); // Use navigate for redirection
-      }, 1000); // Espera 2 segundos antes de redirecionar
+        navigate('/login'); 
+      }, 2000);
 
     } catch (err) {
       console.error(err.response ? err.response.data : err.message);
@@ -39,67 +39,87 @@ const Register = () => {
   };
 
   const handleLoginRedirect = () => {
-    navigate('/Login'); // Redirect to login page
+    navigate('/login'); 
   };
 
   return (
     <>
-      <div className="register-container">
-        <div className="icon-container-register">
+      <div className="illumination top-left"></div>
+      <div className="illumination bottom-right"></div>
+      <div className="page-container">
+        <div className="icon-container">
+          <svg xmlns="http://www.w3.org/2000/svg" width="60" height="61" viewBox="0 0 60 61" fill="none">
+            <circle cx="30" cy="30.9854" r="30" fill="#26273B"/>
+          </svg>
           <FontAwesomeIcon icon={faUser} className="icon" />
         </div>
-        <form onSubmit={onSubmit} className="register-form">
-          <h2 className="register-title">CRIAR CONTA</h2>
-          <input
-            type="text"
-            name="nome"
-            value={nome}
-            onChange={onChange}
-            placeholder="Nome"
-            required
-            className="register-input"
-          />
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={onChange}
-            placeholder="Endereço de e-mail"
-            required
-            className="register-input"
-          />
-          <input
-            type="password"
-            name="senha"
-            value={senha}
-            onChange={onChange}
-            placeholder="Senha"
-            required
-            className="register-input"
-          />
-          <input
-            type="text"
-            name="cpf"
-            value={cpf}
-            onChange={onChange}
-            placeholder="CPF"
-            required
-            className="register-input"
-          />
-          <input
-            type="text"
-            name="telefone"
-            value={telefone}
-            onChange={onChange}
-            placeholder="Telefone"
-            required
-            className="register-input"
-          />
-          <button type="submit" className="register-button">Criar Conta</button>
-          <h2 className="have-account">Ja tem uma conta?</h2>
-          <button onClick={handleLoginRedirect} className="have-account-button">Entrar</button>
-          {message && <p className="success-message">{message}</p>}
-        </form>
+        <div className="register-container">
+          <div className="background-rectangle">
+            <form onSubmit={onSubmit} className="register-form">
+              <h2 className="register-title">CRIAR CONTA</h2>
+              <div className="input-group">
+                <input
+                  type="text"
+                  name="nome"
+                  value={nome}
+                  onChange={onChange}
+                  placeholder="Nome"
+                  required
+                  className="register-input"
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={onChange}
+                  placeholder="E-mail"
+                  required
+                  className="register-input"
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type="password"
+                  name="senha"
+                  value={senha}
+                  onChange={onChange}
+                  placeholder="Senha"
+                  required
+                  className="register-input"
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type="text"
+                  name="cpf"
+                  value={cpf}
+                  onChange={onChange}
+                  placeholder="CPF"
+                  required
+                  className="register-input"
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type="text"
+                  name="telefone"
+                  value={telefone}
+                  onChange={onChange}
+                  placeholder="Telefone"
+                  required
+                  className="register-input"
+                />
+              </div>
+              <button type="submit" className="register-button">Criar conta</button>
+              {message && <p className="success-message">{message}</p>}
+            </form>
+            <div className="have-account">
+              Já possui uma conta? <button onClick={handleLoginRedirect} className="have-account-button">Entrar</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
